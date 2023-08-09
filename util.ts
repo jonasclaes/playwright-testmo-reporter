078a2574912f42c3abc71b007a54b8aadbf30eec
Jonas Claes <jonas@jonasclaes.be>
2023-08-09 21:54:46 +0200

    Added a comment about where the code came from

--- util.ts
+++ util.ts
@@ -1,3 +1,28 @@
+/**
+ * The code that is in this file was largely copied from the Playwright test reporter.
+ * The code was copied to allow for custom formatting of the test results.
+ * The original code can be found here:
+ * https://github.com/microsoft/playwright/blob/fe5cb1603b10871e709eb4e5318333d036635af0/packages/playwright-test/src/reporters/base.ts
+ *
+ * This code was copied to mimic the JUnit reporter error formatting.
+ *
+ * You can find the Microsoft license below:
+ *
+ * > Copyright (c) Microsoft Corporation.
+ * >
+ * > Licensed under the Apache License, Version 2.0 (the "License");
+ * > you may not use this file except in compliance with the License.
+ * > You may obtain a copy of the License at
+ * >
+ * > http://www.apache.org/licenses/LICENSE-2.0
+ * >
+ * > Unless required by applicable law or agreed to in writing, software
+ * > distributed under the License is distributed on an "AS IS" BASIS,
+ * > WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * > See the License for the specific language governing permissions and
+ * > limitations under the License.
+ */
+
 import { FullConfig } from "@playwright/test";
 import {
   TestCase,
