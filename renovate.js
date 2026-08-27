2a93d4574c1a732aebb61864cc0f9f1b8c543356
Jonas Claes <jonas@jonasclaes.be>
2026-08-27 22:19:16 +0200

    chore: add renovate

---
+++ renovate.js
@@ -0,0 +1,9 @@
+module.exports = {
+  autodiscover: true,
+  onboardingConfig: {
+    $schema: "https://docs.renovatebot.com/renovate-schema.json",
+    extends: ["config:recommended"],
+  },
+  optimizeForDisabled: true,
+  persistRepoData: true,
+};
